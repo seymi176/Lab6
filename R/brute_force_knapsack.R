1ee7b46326712978336e8dede69d671dce3ab6b9
aydinardalan <aydin.ardalan@gmail.com>
2022-10-03 00:43:43 +0200

    Parallelize brute force search

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -3,7 +3,7 @@ set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 2000
 knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
 
-#' Knapsakh brute force search
+#' Knapsack brute force search
 #'
 #' @param x A data frame containing values and weights of items
 #' @param W The knapsack's capacity
@@ -11,8 +11,8 @@ knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=run
 #' @return A data frame of two elements: maximum value and selected elements.
 #' @export brute_force_knapsack
 
-brute_force_knapsack <- function(x, W){
-# Check input
+brute_force_knapsack <- function(x,W,parallel=FALSE) {
+  # Check input
   if (!is.data.frame(x)) {stop("x must be a data frame")}
   if (any(x<0 , na.rm = TRUE)) {stop("x items must be positive")}
   if (!(length(x)==2)) {stop("x must have two variables")}
@@ -24,25 +24,57 @@ brute_force_knapsack <- function(x, W){
   tempValue  <- 0
   finalWeight <- 0
   finalValue  <- 0
-  element <- c()
-  sizeOfLoop <- 2^(dim(x[1])[1])
+  elements <- c()
+  n<- dim(x[1])[1]
+  sizeOfLoop <- 2^n
   
-  while(i < sizeOfLoop){
+  if(parallel==TRUE){
+    cores <- parallel::detectCores()
+    cl <- parallel::makeCluster(cores, type = "PSOCK")
     
-    tempWeight <- sum(x$w[as.logical(intToBits(i))])
-    tempValue  <- sum(x$v[as.logical(intToBits(i))])
-    
-    if(tempWeight <= W & tempValue >= finalValue){
+    parallel::clusterExport(cl, varlist=c("x","W","n","elements","finalValue","tempValue", "tempWeight","finalWeight", "sizeOfLoop"), envir=environment())
+    # parallel::clusterEvalQ(cl, library(utils))
+    values<-parallel::parLapply(cl, 1:n, function(i, x,W) {
+      while(i < sizeOfLoop){
+        tempWeight <- sum(x$w[as.logical(intToBits(i))])
+        tempValue  <- sum(x$v[as.logical(intToBits(i))])
+        
+        if(tempWeight <= W & tempValue >= finalValue){
+          
+          finalValue  <- tempValue
+          finalWeight <- tempWeight
+          elements <- which(intToBits(i)==1)
+        }
+        i <- i + 1
+      }
       
-      finalValue  <- tempValue
-      finalWeight <- tempWeight
-      element <- which(intToBits(i)==1)
-    }
+      return(list(value=round(finalValue),elements=elements))
+      
+    }, x, W )
+    value<-values[[1]]["value"]
+    elements<-values[[1]]["elements"]
     
-    i <- i + 1
+    df <- list(value, elements)
+    names(df) <- c("value", "elements")
+    return(df)
+    parallel::stopCluster(cl)
+    
+  }else {
+    while(i < sizeOfLoop){
+      
+      tempWeight <- sum(x$w[as.logical(intToBits(i))])
+      tempValue  <- sum(x$v[as.logical(intToBits(i))])
+      
+      if(tempWeight <= W & tempValue >= finalValue){
+        
+        finalValue  <- tempValue
+        finalWeight <- tempWeight
+        element <- which(intToBits(i)==1)
+      }
+      i <- i + 1
+    }
+    df <- list(finalValue, element)
+    names(df) <- c("value", "elements")
+    return(df)
   }
-  
-  df <- list(finalValue, element)
-  names(df) <- c("value", "elements")
-  return(df)
 }
