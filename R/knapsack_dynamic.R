5d868a3e82e60e7bd53dfa1e9b593ef74fd1a7fc
aydinardalan <aydin.ardalan@gmail.com>
2022-10-02 08:40:58 +0200

    add check input

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -1,16 +1,14 @@
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
 set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
-
+knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
 
 knapsack_dynamic <- function(x, W){
+  if (!is.data.frame(x)) {stop("x must be a data frame")}
+  if (any(x<0 , na.rm = TRUE)) {stop("x items must be positive")}
+  if (!(length(x)==2)) {stop("x must have two variables")}
+  if(!(all(names(x)==c("w","v")))) {stop("x variable names must be u and v")}
+  if (!(W>0 && length(W)==1 && is.numeric(W))) {stop("W must be a positive number")}
   
   m <- matrix(0, length(x$v)+1, W+1)
   
