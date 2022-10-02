6ca7fe812ad81ed2e700d8563bfa178151deac3b
aydinardalan <aydin.ardalan@gmail.com>
2022-10-02 10:15:23 +0200

    Documenting brute_force_knapsack function

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -1,15 +1,25 @@
-RNGversion(min(as.character(getRversion()),"3.5.3"))
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
 set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 2000
 knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
 
+#' Knapsakh brute force search
+#'
+#' @param x A containing values and weights of items
+#' @param W The knapsack's capacity
+#'
+#' @return A data frame of two elements: maximum value and selected elements.
+#' @export brute_force_knapsack
+#'
+#' @examples 
+#' 
 brute_force_knapsack <- function(x, W){
+# Check input
   if (!is.data.frame(x)) {stop("x must be a data frame")}
   if (any(x<0 , na.rm = TRUE)) {stop("x items must be positive")}
   if (!(length(x)==2)) {stop("x must have two variables")}
   if(!(all(names(x)==c("w","v")))) {stop("x variable names must be u and v")}
   if (!(W>0 && length(W)==1 && is.numeric(W))) {stop("W must be a positive number")}
-  
   
   i <- 1
   tempWeight <- 0
