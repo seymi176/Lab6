dd0d2252d93378e9aaf4a18857b03b305d01404f
aydinardalan <aydin.ardalan@gmail.com>
2022-10-03 11:22:12 +0200

    fix bugs

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -27,54 +27,57 @@ brute_force_knapsack <- function(x,W,parallel=FALSE) {
   elements <- c()
   n<- dim(x[1])[1]
   sizeOfLoop <- 2^n
-  
+
   if(parallel==TRUE){
     cores <- parallel::detectCores()
     cl <- parallel::makeCluster(cores, type = "PSOCK")
     
     parallel::clusterExport(cl, varlist=c("x","W","n","elements","finalValue","tempValue", "tempWeight","finalWeight", "sizeOfLoop"), envir=environment())
     # parallel::clusterEvalQ(cl, library(utils))
-    values<-parallel::parLapply(cl, 1:n, function(i, x,W) {
+    values<-parallel::parLapply(cl, 1:sizeOfLoop, function(i, x,W) {
       while(i < sizeOfLoop){
-        tempWeight <- sum(x$w[as.logical(intToBits(i))])
-        tempValue  <- sum(x$v[as.logical(intToBits(i))])
+        comb=which(intToBits(i)==1)
+        tempWeight <- sum(x$w[comb])
+        tempValue <- sum(x$v[comb])
         
-        if(tempWeight <= W & tempValue >= finalValue){
-          
+        if(tempWeight <= W && tempValue > finalValue) {
           finalValue  <- tempValue
-          finalWeight <- tempWeight
-          elements <- which(intToBits(i)==1)
+          elements <- comb
         }
         i <- i + 1
       }
-      
-      return(list(value=round(finalValue),elements=elements))
-      
+      df <- list(round(finalValue), elements)
+      names(df) <- c("value", "elements")
+      return(df)
     }, x, W )
     value<-values[[1]]["value"]
     elements<-values[[1]]["elements"]
-    
+
     df <- list(value, elements)
     names(df) <- c("value", "elements")
     return(df)
     parallel::stopCluster(cl)
+
+    return(c(value,elements))
+    parallel::stopCluster(cl)
     
   }else {
     while(i < sizeOfLoop){
+      comb=which(intToBits(i)==1)
+      tempWeight <- sum(x$w[comb])
+      tempValue <- sum(x$v[comb])
       
-      tempWeight <- sum(x$w[as.logical(intToBits(i))])
-      tempValue  <- sum(x$v[as.logical(intToBits(i))])
-      
-      if(tempWeight <= W & tempValue >= finalValue){
-        
+      if(tempWeight <= W && tempValue >= finalValue) {
         finalValue  <- tempValue
-        finalWeight <- tempWeight
-        element <- which(intToBits(i)==1)
+        elements <- comb
       }
       i <- i + 1
     }
-    df <- list(finalValue, element)
+    df <- list(round(finalValue), elements)
     names(df) <- c("value", "elements")
     return(df)
   }
 }
+# brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel=FALSE)
+# brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel=TRUE)
+  
