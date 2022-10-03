874f0b454f36f132e651d34a6d68b4bc8dcbf097
aydinardalan <aydin.ardalan@gmail.com>
2022-10-03 19:29:18 +0200

    fix bugs

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,20 +1,20 @@
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
 set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
+knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000))
 
-
-greedy_knapsack <- function(x, W){
+greedy_knapsack <- function(x, W) {
+# Input Check
+  if (!is.data.frame(x)) {stop("x must be a data frame")}
+  if (any(x<0 , na.rm = TRUE)) {stop("x items must be positive")}
+  if (!(length(x)==2)) {stop("x must have two variables")}
+  if(!(all(names(x)==c("w","v")))) {stop("x variable names must be u and v")}
+  if (!(W>0 && length(W)==1 && is.numeric(W))) {stop("W must be a positive number")}
   
-  valueWeigthRation  <- x$v / x$w
-  positionOfElements <- c(1:length(x$v))
-  finalDataFrame     <- cbind(x, valueWeigthRation,  positionOfElements)
-  sortedDataFrame    <- finalDataFrame[order(finalDataFrame[,3], decreasing = T),]
+  x$ValPerWei <- x$v / x$w
+  x$Position <- c(1:length(x$v))
+  x <- x[order(x[,3], decreasing = T),]
+  row.names(x) <- c(1:length(x$v))
   
   i <- 1
   tempValue   <- 0
@@ -22,22 +22,22 @@ greedy_knapsack <- function(x, W){
   checkWeight <- 0
   elementList <- c()
   
+  
   while(i < length(x$v)){
     
-    checkWeight <- tempWeight + sortedDataFrame[i,1]
+    checkWeight <- tempWeight + x[i,1]
     if (checkWeight <= W) {
-      tempWeight <- sortedDataFrame[i,1] + tempWeight
-      tempValue  <- sortedDataFrame[i,2] + tempValue
-      elementList <- append(elementList, sortedDataFrame[i,4])
+      tempWeight <- x[i,1] + tempWeight
+      tempValue  <- x[i,2] + tempValue
+      elementList <- append(elementList, x[i,4])
+    } else {
+      break
+      tempValue <- sum( x[1,2]: x[i,2])
     }
-    
-    checkWeight <- 0
     i <- i + 1
   }
   
-  df <- list(tempWeight, tempValue, elementList)
+  df <- list(tempWeight, round(tempValue), elementList)
   names(df) <- c("weight", "value", "elements")
   return(df)
-  
 }
-
